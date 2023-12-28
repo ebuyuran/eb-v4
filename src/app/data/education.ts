d20e15340ad0fdd861f65ddf1ecfd714056c29e9
ebuyuran <efebuyuran@gmail.com>
2023-12-28 20:25:24 +0300

    education for resume

---
+++ src/app/data/education.ts
@@ -0,0 +1,26 @@
+export const education = [
+  {
+    name: 'ISTANBUL COMMERCE UNIVERSITY',
+    field: 'Visual Communication',
+    period: '2009 - 2015',
+    studied: [
+      'Advertisement, Typography, Web Development, Graphic Design, Animation, Screenwriting, Cinema, Photography',
+    ],
+  },
+  {
+    name: 'YELKIKANAT VOCATIONAL HIGH SCHOOL',
+    field: 'Computer',
+    period: '2004 - 2009',
+    studied: ['Basic Programming, Flash, Photoshop'],
+  },
+  {
+    name: 'ISTEK ACIBADEM COLLEGE',
+    field: 'Middle School',
+    period: '2000 - 2004',
+  },
+  {
+    name: 'EYUBOGLU COLLEGE',
+    field: 'Primary Education',
+    period: '1995 - 2000',
+  },
+];
